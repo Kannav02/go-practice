3834dd944f50a5f52b2a4f0ad7001505889f0e14
Kannav02 <kannavsethi02@gmail.com>
2024-10-19 21:30:43 -0400

    added initial files

---
+++ main.go
@@ -0,0 +1,9 @@
+// needs to start with a package name, it is special because it states that the file bleongs to an executable
+package main
+
+// import statement
+import "fmt"
+
+func main() {
+	fmt.Print("Hello From Kannav!!")
+}
---
+++ notes.md
@@ -0,0 +1,9 @@
+# GoLang Tutorial
+
+
+used for multi-threaded applications capable of running on multiple cores
+
+compiled language
+
+scalable and fast systems
+
